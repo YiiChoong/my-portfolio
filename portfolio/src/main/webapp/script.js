7d37dda33ed6c9511e6feda09421d5c843f08906
Yii <yiichoongk@gmail.com>
2021-06-10 15:12:44 +0000

    fetch tutorial

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -37,3 +37,11 @@ window.addEventListener("scroll", ()=>{
         }
     })
 })
+
+async function showHello(){
+    const responseFromServer = await fetch("/hello");
+    const textFromResponse = await responseFromServer.text();
+
+    const helloContainer = document.getElementById("hello-container");
+    helloContainer.innerText = textFromResponse;
+}
